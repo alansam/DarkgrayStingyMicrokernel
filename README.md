410cfb4a80dab9be1b162837d1f10f247eb4609b
Alan Sampson <alansamps@gmail.com>
2020-07-27 19:37:54 +0000

    Add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# DarkgrayStingyMicrokernel
+
+Code fragment from[repl.it](https://repl.it/):
+
+- https://repl.it/@AlanSampson/DarkgrayStingyMicrokernel#main.cpp
--- main.cpp
+++ main.cpp
@@ -1,48 +1,21 @@
-#include <iostream>
-#include <iomanip>
-#include <memory>
-#include <cstdint>
+#include <cstdio>
+
+struct test_s { char name[0x10]; };
+class test_c { char name[0x10]; };
 
 int main() {
 
-  double hoursWorked;
-  double rateOfPay;
-  double overtimeHours;
-  double salary;
-  double overtime;
+  int ry[5] = { 3, 4, };
+  for (size_t r = 0; r < 5; ++r) {
+    printf("%d ", ry[r]);
+  }
+  putchar('\n');
 
-  std::cout << std::showpoint << std::setprecision(2) << std::fixed;
+  test_s st;
+  test_c ct;
 
-#undef INTERACTIVE_D07
-#ifdef INTERACTIVE_D07
-  std::cout << "hours worked " << std::endl;
-  std::cin >> hoursWorked;
-
-  std::cout << std::setw(2) << "pay rate " << std::endl;
-  std::cin >> rateOfPay;
-
-  std::cout << std::setw(3) << "overtime " << std::endl;
-  std::cin >> overtimeHours;
-  std::cout << std::endl;
-#else
-  hoursWorked = 40.0;
-  rateOfPay = 25.0;
-  overtimeHours = 3.0;
-#endif
-
-  overtime = overtimeHours * rateOfPay * 1.5;
-
-  std::cout << std::setw(8) << hoursWorked << std::endl;
-  std::cout << std::setw(8) << overtimeHours << std::endl;
-  std::cout << std::setw(8) << rateOfPay << std::endl;
-  std::cout << std::setw(8) << rateOfPay * hoursWorked << std::endl;
-  std::cout << std::setw(8) << overtime << std::endl;
-
-  salary = rateOfPay * hoursWorked + overtime;
-
-  std::cout << std::setw(8)
-            << salary
-            << std::endl;
+  printf("%p %zx\n", &st, sizeof(st));
+  printf("%p %zx\n", &ct, sizeof(ct));
 
   return 0;
 }
